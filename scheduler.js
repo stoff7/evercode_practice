8368d924665df41c6b5e30303b5e0df1d395600e
stoff7 <novikov.stas2005@yandex.ru>
2026-05-15 21:00:04 +0300

    Changed logger to new

--- scheduler.js
+++ scheduler.js
@@ -1,21 +1,23 @@
 const { InvalidIntervalError } = require('./errors');
-const createLogger = require('./logger');
+const Logger = require('./logger');
+require('dotenv').config();
 
-const logger = createLogger();
+const logger = new Logger(process.env.LOG_LEVEL || 'info');
 
-logger('Scheduler module loaded.');
+logger.info('Scheduler module loaded.');
 
 const schedule = (name, interval, task) => {
     if (interval < 1000) throw new InvalidIntervalError('Interval must be greater than 1000 milliseconds.');
-    logger(`Scheduling task "${name}" to run every ${interval} milliseconds.`);
+    logger.info(`Scheduling task "${name}" to run every ${interval} milliseconds.`);
     setInterval(() => {
-        logger(`Executing task "${name}".`);
+        logger.debug('Executing scheduled task...', `TASK=${name}`);
+        logger.info(`Executing task "${name}".`);
         task();
     }, interval);
 };
 
 const runningTask = () => {
-    logger('Running...');
+    logger.info('Running...', 'RUNNING_TASK ID=12345');
 }
 
 schedule('Running Task', 10_000, runningTask);
