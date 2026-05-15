20f41d7f60bcb49aa36c1cf2291850115a569b6a
stoff7 <novikov.stas2005@yandex.ru>
2026-05-15 20:57:48 +0300

    Added logger levels

--- logger.js
+++ logger.js
@@ -1,30 +1,48 @@
 const { appName, version } = require('./config');
 const { InvalidMessageError } = require('./errors');
+const LEVELS = { trace: 0, debug: 1, info: 2, warn: 3, error: 4 };
 
 
+/**
+ * Simple logger class
+ * @description Logs messages with different levels and timestamps
+ */
 class Logger {
-    constructor() { }
+    #minLevel;
+    /**
+     * @constructor
+     * @param {string} minLevel Minimum log level to output 
+     * LEVELS = { trace: 0, debug: 1, info: 2, warn: 3, error: 4 };
+     */
+    constructor(minLevel) {
+        this.#minLevel = LEVELS[minLevel];
+    }
 
     #log(message, level, requestId) {
+        if (LEVELS[level] < this.#minLevel) return;
         const timestamp = new Date().toISOString();
-        if (!message) throw new InvalidMessageError('Message is required for logging.');
         console.log(`[${timestamp}] ${appName} ${version} [${level}] ${requestId || ''} ${message}`)
     }
 
     info(message, requestId) {
-        this.#log(message, 'INFO', requestId)
+        if (!message) throw new InvalidMessageError('Message is required for logging.');
+        this.#log(message, 'info', requestId)
     }
     warn(message, requestId) {
-        this.#log(message, 'WARN', requestId)
+        if (!message) throw new InvalidMessageError('Message is required for logging.');
+        this.#log(message, 'warn', requestId)
     }
     error(message, requestId) {
-        this.#log(message, 'ERROR', requestId)
+        if (!message) throw new InvalidMessageError('Message is required for logging.');
+        this.#log(message, 'error', requestId)
     }
     trace(message, requestId) {
-        this.#log(message, 'TRACE', requestId)
+        if (!message) throw new InvalidMessageError('Message is required for logging.');
+        this.#log(message, 'trace', requestId)
     }
     debug(message, requestId) {
-        this.#log(message, 'DEBUG', requestId)
+        if (!message) throw new InvalidMessageError('Message is required for logging.');
+        this.#log(message, 'debug', requestId)
     }
 
 }
