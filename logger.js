9abf6e459663deb07f7a335e801afbc43bf08040
stoff7 <novikov.stas2005@yandex.ru>
2026-05-15 20:13:32 +0300

    Logger refactored

--- logger.js
+++ logger.js
@@ -1,11 +1,32 @@
 const { appName, version } = require('./config');
 const { InvalidMessageError } = require('./errors');
 
-const createLogger = () => {
-    return (message) => {
-        if (!message) throw new InvalidMessageError('Message is required for logging.');
-        console.log(`[${appName} v${version}] ${message}`);
-    };
-};
 
-module.exports = createLogger;
+class Logger {
+    constructor() { }
+
+    #log(message, level, requestId) {
+        const timestamp = new Date().toISOString();
+        if (!message) throw new InvalidMessageError('Message is required for logging.');
+        console.log(`[${timestamp}] ${appName} ${version} [${level}] ${requestId || ''} ${message}`)
+    }
+
+    info(message, requestId) {
+        this.#log(message, 'INFO', requestId)
+    }
+    warn(message, requestId) {
+        this.#log(message, 'WARN', requestId)
+    }
+    error(message, requestId) {
+        this.#log(message, 'ERROR', requestId)
+    }
+    trace(message, requestId) {
+        this.#log(message, 'TRACE', requestId)
+    }
+    debug(message, requestId) {
+        this.#log(message, 'DEBUG', requestId)
+    }
+
+}
+
+module.exports = Logger;
